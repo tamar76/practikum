42120cc86ded83de7263c9ad5d726492bca2ef96
tamar76 <117015870+tamar76@users.noreply.github.com>
2023-03-14 17:38:15 +0200

    Add files via upload

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pract.common.DTO_s;
+//using Pract.Repository.Entities;
+using Pract.Services;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IService<UserDTO> _User;
+        public UserController(IService<UserDTO> User)
+        {
+            _User = User;
+        }
+      
+       
+    
+       
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        [Route("Post")]
+        public async Task<List<UserDTO>> Post([FromBody] List<UserDTO> value)
+        {
+            return await _User.Add(value);
+        }
+
+    }
+}
