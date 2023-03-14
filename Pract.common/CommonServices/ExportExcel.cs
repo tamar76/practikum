7103d9fe5c86f88b587d2b1cbb44cade2612a714
tamar76 <117015870+tamar76@users.noreply.github.com>
2023-03-14 17:39:03 +0200

    Add files via upload

---
+++ Pract.common/CommonServices/ExportExcel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract.Common.CommonServices
+{
+    class ExportExcel
+    {
+        //public void Export(List<User> data)
+        //{
+        //    foreach (var item in Object.Keys(User))
+        //    {
+        //        foreach (var user in data)
+        //        {
+        //            workSheet[]=user[item]
+        //        }
+        //    } 
+        //    {
+
+        //    }
+
+        //}
+    }
+}
---
+++ Pract.common/DTO's/UserDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract.common.DTO_s
+{
+    public  class UserDTO
+    {
+        public int Id { get; set; }
+        public string Identity { get; set; }
+        public string ParentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string MaleOrFemale { get; set; }
+        public string  HMO { get; set; }
+    }
+
+}
